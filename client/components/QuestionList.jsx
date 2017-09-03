37cd9ac7ce1949f10aa6f87bd203c2db4cb2e55a
simonol1 <simondanolearyis@gmail.com>
2017-09-03 17:39:46 +1000

    styling the page

--- client/components/QuestionList.jsx
+++ client/components/QuestionList.jsx
@@ -5,11 +5,10 @@ const QuestionList = (props) => {
   let q = props.question
   return (
     <div>
-      <h4>{q.title}?</h4>
-      <p><img src={q.owner.profile_image}/></p>
+      <p><img src={q.owner.profile_image}/><h4>{q.title}?</h4></p>
       <p>Number of answers: {q.answer_count}</p>
-      <p>Tags: {q.tags}</p>
       <p>Score: {q.score}</p>
+      <p>Tags: {q.tags}</p>
       <a href={q.link}>{q.link}</a>
       <hr></hr>
     </div>
